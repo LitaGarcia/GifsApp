b6423d9abeb6165d548f14515e6b2849c582ca70
Manuela Garcia <manuela.garcia@bosonit.com>
2022-11-28 12:13:28 +0100

    fetch giphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -9,6 +10,9 @@ export class GifsService {
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) {}
+
   public buscarGifs(query: string) {
     query = query.trim().toLocaleLowerCase();
     if (!this._historial.includes(query)) {
@@ -16,13 +20,12 @@ export class GifsService {
     }
     this._historial = this._historial.splice(0, 10); //limitar a 10 el historial
     console.log(this._historial);
-
-    fetch(
-      'https://api.giphy.com/v1/gifs/search?api_key=qeaKU5vAqOakhKnfGYCsSSa7dyMxrViB&q=dragon ball z&limit=10'
-    ).then((resp) => {
-      resp.json().then((data) => {
-        console.log(data);
+    this.http
+      .get(
+        'https://api.giphy.com/v1/gifs/search?api_key=qeaKU5vAqOakhKnfGYCsSSa7dyMxrViB&q=dragon ball z&limit=10'
+      )
+      .subscribe((resp: any) => {
+        console.log(resp.data);
       });
-    });
   }
 }
