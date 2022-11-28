a249e3aa00ed0a2d651f85a6e9eee7960356b844
Manuela Garcia <manuela.garcia@bosonit.com>
2022-11-28 11:35:25 +0100

    control of historial

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,13 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class GifsService {
-  
   private _historial: string[] = [];
   get historial() {
     return [...this._historial];
   }
   public buscarGifs(query: string) {
-    this._historial.unshift(query);
+    query = query.trim().toLocaleLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+    }
+    this._historial = this._historial.splice(0, 10); //limitar a 10 el historial
     console.log(this._historial);
   }
 }
